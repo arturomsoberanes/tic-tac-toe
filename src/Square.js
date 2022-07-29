cb09f9c291bef30c9903bcc219ec37ec4d8efa6d
Arturo Soberanes <arturo.soberanes01@gmail.com>
2022-07-29 13:24:37 -0500

    Add components and logic

---
+++ src/Square.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Square.css';
+
+function Square({ id, newState }){
+  const [color, setColor] = React.useState('green');
+  const [status, setStatus] = React.useState(null);
+  const XorO = ["O","X"];
+
+  const palet = ['red', 'blue', 'green'];
+  const getRandomColor = () => palet[Math.floor(Math.random()*3)];
+
+  React.useEffect( () => {
+    console.log(`Render ${id}`);
+    return () => console.log(`unmounting square ${id}`);
+  });
+  const turn = e => {
+    let col = getRandomColor();
+    setColor(col);
+    let nextPlayer = newState(id);
+    setStatus(nextPlayer);
+    e.target.style.background = col;
+  };
+
+  return (
+    <button onClick={turn}>
+      <h1>{XorO[status]}</h1>
+    </button>
+  );
+}
+
+export default Square;
+
